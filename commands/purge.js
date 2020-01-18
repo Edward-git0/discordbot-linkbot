93421a5efef4b5becce6aad33cf73ee217bc3029
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-18 16:39:05 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -2,20 +2,22 @@ module.exports = {
     name: "purge",
     description: "Removes messages",
     execute(message, args) {
-        if (!message.member.hasPermission(["MANAGE_MESSAGES"]))
-        return message.channel
-          .send("You can't perform this command! :x:")
-          .then((msg) => {
-            msg.delete(5000);
-          });
+        if (!message.member.hasPermission(["MANAGE_MESSAGES"])) {
+            return message.channel
+            .send("You can't perform this command! :x:")
+            .then((msg) => {
+                msg.delete(5000);
+            });
+        }
   
-      const deleteCount = parseInt(args[0], 10);
-        message.reply("args[0]: "+args[0]);
-      if (!deleteCount || deleteCount < 1 || deleteCount > 100)
-        return message.reply(
-          "Please provide a number between 2 and 100 for the number of messages to delete. :x:"
-        );
+        const deleteCount = parseInt(args[0], 10);
+        if (!deleteCount || deleteCount < 1 || deleteCount > 100) {
+            return message.reply(
+            "Please provide a number between 2 and 100 for the number of messages to delete. :x:"
+            );
+      } else { message.reply("args[0]: "+args[0]); }
   
+      
       const fetched = message.channel.fetchMessages({
         limit: deleteCount + 1
       });
