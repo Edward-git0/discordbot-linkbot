0aa37d810a7e16786d1ee4b582bda37db773c443
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-18 17:31:11 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -21,31 +21,8 @@ module.exports = {
             );
       }
 
-      const logs = message.guild.channels.find("name", "infraction-logs");
-/*      let logsembed = new Discord.RichEmbed()
-      .setTitle("Logs")
-      .setColor("BLURPLE")
-      .addField("Purge", "purge") //message.author+" purged "+deleteCount+" messages in "+message.channel+"! :white_check_mark:")
-      .setTimestamp() */
-
-      const exampleEmbed = new Discord.RichEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addField('Regular field title', 'Some value here')
-	.addBlankField()
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-
-
-      message.channel.bulkDelete(deleteCount+1).then((messages) => logs.send('exampleEmbed'))
+      let logs = message.guild.channels.find("name", "infraction-logs");
+      message.channel.bulkDelete(deleteCount+1).then(messages => logs.send('logsembed'))
 
     },
 }
