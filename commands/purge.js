c8f0fed48268b45107a9eeb7f0befbb7f51b95b6
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-18 17:21:37 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -24,14 +24,13 @@ module.exports = {
       const logsembed = new Discord.RichEmbed()
       .setTitle("Logs")
       .setColor("BLURPLE")
-      .addField("Purge", message.author+" purged "+deleteCount+" messages in "+message.channel+"! :white_check_mark:")
+      .addField("Purge", "purge") //message.author+" purged "+deleteCount+" messages in "+message.channel+"! :white_check_mark:")
       .setThumbnail(message.author.avatarURL)
       .setTimestamp()
       .setFooter(`User: ${message.author.username}`, message.author.avatarURL);
 
-      client.channels.get("668197255366574132").send(logsembed);
 
-      message.channel.bulkDelete(deleteCount+1);
+      message.channel.bulkDelete(deleteCount+1).then(messages => logs.send(logsembed))
 
     },
 }
