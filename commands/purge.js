90a2d70cd7deae0e7fe6e4bd62c2cb90f06c9678
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-18 17:14:50 -0500

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -20,6 +20,17 @@ module.exports = {
             "Please provide a number between 2 and 100 for the number of messages to delete. :x:"
             );
       }
+
+      const logsembed = new Discord.RichEmbed()
+      .setTitle("Logs")
+      .setColor("BLURPLE")
+      .addField("Purge", message.author+" purged "+deleteCount+" messages in "+message.channel+"! :white_check_mark:")
+      .setThumbnail(message.author.avatarURL)
+      .setTimestamp()
+      .setFooter(`User: ${message.author.username}`, message.author.avatarURL);
+
+      client.channels.get("668197255366574132").send('logsembed');
       message.channel.bulkDelete(deleteCount+1);
+
     },
 }
