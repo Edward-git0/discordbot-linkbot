1a8d96c7f345cc96fc2c18a448bb139c710b1fb8
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-18 23:31:43 -0500

    Update lfg.js

--- commands/lfg.js
+++ commands/lfg.js
@@ -24,8 +24,15 @@ module.exports = {
             msg.react("👌");
             const filter = (reaction, user) => reaction.emoji.name === '👌' && user.id === message.author.id
             const collector = msg.createReactionCollector(filter, { time: 3600000 });
-            collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
-            collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+            collector.on('collect', r => { 
+                const embed3 = new Discord.RichEmbed()
+                .setTitle("Looking For Group")
+                .addField(game, `${message.author.toString()} found a group for ${game}!`)
+                .setTimestamp()
+                .setThumbnail(message.author.avatarURL)
+                .setColor("BLURPLE")
+                msg.edit(embed3);
+             });
             message.channel.send(embed2).then((msg) => {
             msg.delete(1000);
         }) })
