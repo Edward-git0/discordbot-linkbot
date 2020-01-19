1a1d8021f722052a5b0edc60ebd68b8ffde9819b
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-18 23:20:11 -0500

    Update lfg.js

--- commands/lfg.js
+++ commands/lfg.js
@@ -21,7 +21,7 @@ module.exports = {
         
         const m = await message.guild.channels.get("668131178900881428").send(lfg).then((msg) => { 
             msg.react("👌").then((reaction) => {
-                console.log(typeof(reaction));
+                console.log(reaction);
             }); 
             message.channel.send(embed2).then((msg) => {
             msg.delete(1000);
